4c6d85321e9856b1abe888c2678b3039f824ec33
アンテナレコード <jounan.bishokukai@yahoo.co.jp>
2025-04-30 10:18:29 +0900

    feat: Slack通知とNetlify連携の最終調整

--- src/components/results/EnhancedResultScreen.js
+++ src/components/results/EnhancedResultScreen.js
@@ -6,6 +6,7 @@ import QuickConsultationForm from './QuickConsultationForm';
 import { trackContactStart, trackRestart, trackShare } from '../../utils/analytics';
 import { Chart as ChartJS, RadialLinearScale, PointElement, LineElement, Filler, Tooltip, Legend } from 'chart.js';
 import { Radar } from 'react-chartjs-2';
+import AnimalIcon from '../AnimalIcon';   // ★←追加
 
 // Chart.jsの設定
 ChartJS.register(RadialLinearScale, PointElement, LineElement, Filler, Tooltip, Legend);
